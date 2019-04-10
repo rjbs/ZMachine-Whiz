ca56718f60938865a7ee60ef91d04cc805e9128c
Ricardo Signes <rjbs@cpan.org>
2019-04-10 19:00:53 -0400

    now we use with
    because we want to be more like JavaScript

--- lib/ZMachine/Story.pm
+++ lib/ZMachine/Story.pm
@@ -52,43 +52,43 @@ class ZMachine::Story {
   }
 
   method write-to-file($filename) {
-    my $file  = ZMachine::File.new(filename => $filename);
+    with ZMachine::File.new(filename => $filename) {
+      .fh.print( chr(0) x .filesize );
 
-    $file.fh.print( chr(0) x $file.filesize );
+      ## START HEADER
+      .write-at(0x00, mkbyte($.version));  # story file version
+      .write-at(0x04, mkword(0x1230)); # base address of high memory
+      .write-at(0x06, mkword($.routine-pos('start'))); # PC initial value
+      .write-at(0x08, mkword(0x1000)); # address of dictionary
+      .write-at(0x0A, mkword(0x2000)); # address of object table
+      .write-at(0x0C, mkword(0x3000)); # address of global variables table
+      .write-at(0x0E, mkword(0x4000)); # base address of static memory
+      .write-at(0x12, $.serial.encode('ascii'));    # serial number
+      .write-at(0x18, mkword(0x5000)); # address of abbreviations table
+      .write-at(0x1A, mkword(.filesize / 4)); # length of file (divided by 4, in v5)
 
-    ## START HEADER
-    $file.write-at(0x00, mkbyte($.version));  # story file version
-    $file.write-at(0x04, mkword(0x1230)); # base address of high memory
-    $file.write-at(0x06, mkword($.routine-pos('start'))); # PC initial value
-    $file.write-at(0x08, mkword(0x1000)); # address of dictionary
-    $file.write-at(0x0A, mkword(0x2000)); # address of object table
-    $file.write-at(0x0C, mkword(0x3000)); # address of global variables table
-    $file.write-at(0x0E, mkword(0x4000)); # base address of static memory
-    $file.write-at(0x12, $.serial.encode('ascii'));    # serial number
-    $file.write-at(0x18, mkword(0x5000)); # address of abbreviations table
-    $file.write-at(0x1A, mkword($file.filesize / 4)); # length of file (divided by 4, in v5)
+      .write-at(0x28, mkword(0x6000 / 8)); # routines offset (divided by 8)
+      .write-at(0x2A, mkword(0x7000 / 8)); # static strings offset (divided by 8)
 
-    $file.write-at(0x28, mkword(0x6000 / 8)); # routines offset (divided by 8)
-    $file.write-at(0x2A, mkword(0x7000 / 8)); # static strings offset (divided by 8)
+      .write-at(0x2C, mkbyte(0));      # default bg
+      .write-at(0x2D, mkbyte(1));      # default fg
 
-    $file.write-at(0x2C, mkbyte(0));      # default bg
-    $file.write-at(0x2D, mkbyte(1));      # default fg
+      .write-at(0x2E, mkword(0x8000)); # address of terminating characters table
 
-    $file.write-at(0x2E, mkword(0x8000)); # address of terminating characters table
+      .write-at(0x34, mkword(0x0000)); # address of alphabet table (0 = default)
 
-    $file.write-at(0x34, mkword(0x0000)); # address of alphabet table (0 = default)
+      .write-at(0x36, mkword(0x0000)); # address of header extension table; (0 = none)
+      ## END HEADER
 
-    $file.write-at(0x36, mkword(0x0000)); # address of header extension table; (0 = none)
-    ## END HEADER
+      for %!strings.kv -> $name, $todo {
+        .write-at($todo<pos>, $todo<buf>);
+      }
 
-    for %!strings.kv -> $name, $todo {
-      $file.write-at($todo<pos>, $todo<buf>);
+      for %!routines.kv -> $name, $todo {
+        .write-at($todo<pos>, $todo<code>);
+      }
+
+      .close;
     }
-
-    for %!routines.kv -> $name, $todo {
-      $file.write-at($todo<pos>, $todo<code>);
-    }
-
-    $file.close;
   }
 }
